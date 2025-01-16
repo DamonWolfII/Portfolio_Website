dbd073ab785e17f3cde2f0daf1d3bcb125960e21
Kshitiz <kshitizbabik.37@gmail.com>
2025-01-16 10:52:36 +0545

    Year Change

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,13 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-center mt-7'>
-     <footer>
-    <p>&copy; <span id="year"></span> Kshitij 2025. All Rights Reserved.</p>
-</footer>
-
-<script>
-    document.getElementById("year").textContent = new Date().getFullYear();
-</script>
+      <footer>
+        <p>&copy; Kshitij 2025. All Rights Reserved.</p>
+      </footer>
     </div>
   )
 }
